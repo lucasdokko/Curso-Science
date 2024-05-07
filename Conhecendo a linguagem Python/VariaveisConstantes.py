d25b929bb4ac4a6a8cb6a695ce0f3f9807ad904d
lucasdokko <lucasdokko@hotmail.com>
2024-05-07 00:03:27 -0300

    Conhecendo a linguagem

---
+++ Conhecendo a linguagem Python/VariaveisConstantes.py
@@ -0,0 +1,18 @@
+nome = "Lucas"
+idade = 28
+
+nome, idade = "Marcela", 27
+
+
+print(nome, idade)
+
+
+limite_saque_diario = 3000
+
+BRAZILIAN_STATES = ["SP", "RJ", "SC","RS"]
+
+
+print(BRAZILIAN_STATES)
+
+
+
